3361f8c45c33a3af035015bcf5c0f97ccc5bf0bf
CreamyMilk <creamym254@gmail.com>
2020-12-17 09:20:20 +0300

    🎫 Get app version in the settings pane

--- lib/views/settings_tab.dart
+++ lib/views/settings_tab.dart
@@ -1,11 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:package_info/package_info.dart';
 import 'package:zukes/widgets/getNewAPIdata.dart';
 
-import 'package:zukes/widgets/graph_trans.dart';
+String getAppversion() {
+  //String appName = packageInfo.appName;
+  String rs;
+  PackageInfo.fromPlatform().then((PackageInfo packageInfo) {
+    //String appName = packageInfo.appName;
+    String packageName = packageInfo.packageName;
+    String version = packageInfo.version;
+    String buildNumber = packageInfo.buildNumber;
+    rs = packageName + "/ v:" + version + "/ b:" + buildNumber;
+  });
+
+  return rs;
+}
 
 class SettingsTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    String _version = getAppversion();
     //final MediaQueryData d = MediaQuery.of(context);
     return SafeArea(
         child: RefreshIndicator(
@@ -13,7 +27,11 @@ class SettingsTab extends StatelessWidget {
       child: CustomScrollView(
         slivers: [
           SliverAppBar(
-            title: Text("Terms and Conditions"),
+            expandedHeight: 150,
+            title: Text(
+              "Terms and Conditions",
+              style: TextStyle(color: Colors.black),
+            ),
             backgroundColor: Colors.white,
             elevation: 0.0,
           ),
@@ -27,11 +45,25 @@ class SettingsTab extends StatelessWidget {
                   child: Row(children: [
                     Expanded(
                         flex: 10,
-                        child: Text("Get a daily quite\n of all expnses")),
+                        child: Text(
+                          "Get a daily quite\n of all expnses",
+                          style: TextStyle(fontSize: 30),
+                        )),
                     Expanded(flex: 1, child: Icon(Icons.present_to_all))
                   ]),
                 ),
-                CoolGraph(firestoreKey: "building8"),
+                Spacer(),
+                OutlineButton(
+                    onPressed: null,
+                    child: Text(
+                      "Sign Out",
+                      style: TextStyle(color: Colors.red),
+                    )),
+                SizedBox(height: 10),
+                Text(
+                  "App version: $_version",
+                  style: TextStyle(color: Colors.grey),
+                )
               ],
             ),
           ),
