b38a8e5109e4f5efe7ee8efaaccedc8a136da72e
CreamyMilk <creamym254@gmail.com>
2020-12-22 09:04:35 +0300

    📑List view need an bounded parent yani the need a height

--- lib/widgets/rent_amount_card.dart
+++ lib/widgets/rent_amount_card.dart
@@ -7,21 +7,20 @@ class RentAmountsCard extends StatelessWidget {
   Widget build(BuildContext context) {
     final rBox = Provider.of<RentAmountP>(context);
     return Card(
-      child: Column(
-        children: [
-          ListView(
-            children: [
-              ListTile(
-                leading: Text("Paid"),
-                trailing: Text("Ksh.${rBox.paid}"),
-              ),
-              ListTile(
-                leading: Text("Due"),
-                trailing: Text("Ksh.${rBox.due}"),
-              )
-            ],
-          ),
-        ],
+      child: Container(
+        height: 100,
+        child: ListView(
+          children: [
+            ListTile(
+              leading: Text("Paid"),
+              trailing: Text("Ksh.${rBox.paid}"),
+            ),
+            ListTile(
+              leading: Text("Due"),
+              trailing: Text("Ksh.${rBox.due}"),
+            )
+          ],
+        ),
       ),
     );
   }
