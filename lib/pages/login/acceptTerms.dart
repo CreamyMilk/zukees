3e3bad2e6920e4997e68944873d49c296bbede90
CreamyMilk <creamym254@gmail.com>
2020-12-25 17:21:46 +0300

    🍁Fixes sizing issues and icon sizes

--- lib/pages/login/acceptTerms.dart
+++ lib/pages/login/acceptTerms.dart
@@ -10,7 +10,7 @@ class AcceptTerms extends StatelessWidget {
               top: 16.0, bottom: 16.0, left: 8.0, right: 8.0),
           child: MaterialButton(
             shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(18.0),
+                borderRadius: BorderRadius.circular(20.0),
                 side: BorderSide(color: Colors.green)),
             padding: EdgeInsets.all(8.0),
             color: Colors.green,
@@ -24,12 +24,12 @@ class AcceptTerms extends StatelessWidget {
                 Container(
                   margin: EdgeInsets.only(top: 10, bottom: 10),
                   child: Text("CONTINUE",
-                      style: TextStyle(color: Colors.white, fontSize: 25)),
+                      style: TextStyle(color: Colors.white, fontSize: 20)),
                 ),
                 Spacer(),
                 Icon(
-                  Icons.keyboard_arrow_right,
-                  size: 10,
+                  Icons.arrow_forward,
+                  size: 15,
                   color: Colors.white,
                 ),
               ],
@@ -54,56 +54,59 @@ class AcceptTerms extends StatelessWidget {
                   child: Text("JK")),
               SizedBox(height: 2),
               Text("Jotham Kinyua"),
-
               SizedBox(height: 3),
               Text("+254797678252"),
               Spacer(),
-
               //Add animation to turn to scratch card then submit to verify
-
               Row(children: [
                 // lstore.buttonStates.forEach((k) => Text('$k')),
                 Spacer(),
                 Container(
                     height: 100,
                     width: 100,
-                    color: Colors.green,
+                    color: Color(0xfff43b02a),
                     child: Text("Contaract animation will be here")),
                 Spacer(),
               ]),
-              SizedBox(
-                height: 100,
-              ),
+              Spacer(),
               Container(
                 width: MediaQuery.of(context).size.width,
                 child: RichText(
-                    text: TextSpan(children: [
-                  TextSpan(
-                      style: TextStyle(color: Colors.grey),
-                      text:
-                          "By tapping \"\"Continue\"\" you acknowledge that you have read the"),
-                  TextSpan(
-                    text: " Privacy Policy ",
-                    style: TextStyle(
-                        color: Colors.green, fontWeight: FontWeight.w500),
-                    recognizer: TapGestureRecognizer()
-                      ..onTap = () {
-                        Navigator.of(context).pushNamed("/termspage");
-                      },
+                  text: TextSpan(
+                    children: [
+                      TextSpan(
+                          style: TextStyle(color: Colors.grey, fontSize: 10),
+                          text:
+                              "By tapping \"Continue\" you acknowledge that you have read the"),
+                      TextSpan(
+                        text: " Privacy Policy ",
+                        style: TextStyle(
+                            color: Colors.green[300],
+                            fontWeight: FontWeight.w500,
+                            fontSize: 10),
+                        recognizer: TapGestureRecognizer()
+                          ..onTap = () {
+                            Navigator.of(context).pushNamed("/termspage");
+                          },
+                      ),
+                      TextSpan(
+                        text: "and agree to the",
+                        style: TextStyle(color: Colors.grey, fontSize: 10),
+                      ),
+                      TextSpan(
+                        recognizer: TapGestureRecognizer()
+                          ..onTap = () {
+                            Navigator.of(context).pushNamed("/termspage");
+                          },
+                        text: "Terms and Conditions ",
+                        style: TextStyle(
+                            color: Colors.green[300],
+                            fontSize: 10,
+                            fontWeight: FontWeight.w500),
+                      ),
+                    ],
                   ),
-                  TextSpan(
-                    text: "and agree to the",
-                    style: TextStyle(color: Colors.grey),
-                  ),
-                  TextSpan(
-                      recognizer: TapGestureRecognizer()
-                        ..onTap = () {
-                          Navigator.of(context).pushNamed("/termspage");
-                        },
-                      text: "Terms and Conditions ",
-                      style: TextStyle(
-                          color: Colors.green, fontWeight: FontWeight.w500)),
-                ])),
+                ),
               ),
             ],
           ),
